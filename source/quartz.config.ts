bbb19765c46bd6e2fe272f98d1b7a87ee193d92b
jlightree <jlightree@gmail.com>
2025-01-07 08:58:14 +0900

    Update quartz.config.ts

--- source/quartz.config.ts
+++ source/quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "주빛나무 블로그",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
